1b906e0be6c4a59292e4b936487ac2f50ac9012a
MonTa <monta.steam@gmail.com>
2023-04-11 09:31:40 +0100

    made filename unique

--- models/videoSchema.js
+++ models/videoSchema.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const videoSchema = mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   // aircraft: { type: Schema.Types.ObjectId,ref:'airCraft' },
   aircraft: { type: String, required: true },
   place: { type: String, required: true },
   date: { type: Date, required: true },
   tag: {type: Array,default: [],required: true },
   videoPath: { type: String, required: true },
-  filename : { type: String },
+  filename : { type: String, required: true , unique: true },
   uploaded: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model('Video', videoSchema);
